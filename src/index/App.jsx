2b3cea5816d308601272182bfcbda4bf4200bc3a
enigmawxy <1480973357@qq.com>
2020-07-31 17:32:15 +0800

    8-03

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,29 +1,62 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import './App.css'
+import React, { useCallback, useMemo } from 'react'
+import { connect } from 'react-redux'
+import './App.scss'
 import Header from '../common/Header'
 import DepartDate from './DepartDate'
 import HighSpeed from './HighSpeed'
 import Journey from './Journey'
 import Submit from './Submit'
 
+import { exchangeFromTo, showCitySelector } from './actions'
+import { bindActionCreators } from 'redux'
+
 function App(props) {
+    const { from, to, dispatch } = props
+    const onBack = useCallback(() => {
+        window.history.back()
+    }, [])
+
+    // const doExchangeFromTo = useCallback(() => {
+    //     dispatch(exchangeFromTo())
+    // }, [dispatch])
+
+    // const doShowCitySelector = useCallback((m) => {
+    //     dispatch(showCitySelector(m))
+    // }, [dispatch])
+
+    const cbs = useMemo(()=>{
+        return bindActionCreators({
+            exchangeFromTo,
+            showCitySelector,
+        }, dispatch)
+    },[dispatch])
+
     return (
         <div>
-            <Header/>
-            <Journey/>
-            <DepartDate/>
-            <HighSpeed/>
-            <Submit/>
+            <div className="header-wrapper">
+                <Header title="火车票" onBack={onBack} />
+            </div>
+            <form className="form">
+                <Journey
+                    from={from}
+                    to={to}
+                    {...cbs}
+                    // exchangeFromTo={doExchangeFromTo}
+                    // showCitySelector={doShowCitySelector}
+                />
+                <DepartDate />
+                <HighSpeed />
+                <Submit />
+            </form>
         </div>
     )
 }
 
 export default connect(
     function mapStateToProps(state) {
-        return {}
+        return state
     },
     function mapDispatchToProps(dispatch) {
-        return {}
+        return { dispatch }
     }
 )(App)
