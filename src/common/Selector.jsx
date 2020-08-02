910d219425ff1b7b86eaa652c87cb3c21e471f42
enigmawxy <1480973357@qq.com>
2020-08-02 15:34:47 +0800

    807

--- src/common/Selector.jsx
+++ src/common/Selector.jsx
@@ -1,4 +1,10 @@
-import React, { useState, useMemo, useEffect, memo } from 'react'
+import React, {
+    useState,
+    useMemo,
+    useEffect,
+    memo,
+    useCallback
+} from 'react'
 import './Selector.scss'
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
@@ -12,7 +18,7 @@ const DataItem = memo(function DataItem(props) {
     return (
         <li
             className='data-li'
-            onClick={()=> onSelect(name)}
+            onClick={() => onSelect(name)}
         >{name}</li>
     )
 })
@@ -25,24 +31,24 @@ DataItem.propTypes = {
 const SectionItem = memo(function SectionItem(props) {
     const {
         title,
-        cities =[],
+        cities = [],
         onSelect,
     } = props
 
     return (
         <ul className='data-ul'>
-          <li className='data-li'>{title}</li>
-          {
-              cities.map(city=> {
-                  return (
-                      <DataItem
-                        key={city.name}
-                        name = {city.name}
-                        onSelect={onSelect}
-                      />
-                  )
-              })
-          }
+            <li className='data-li' data-cate={title}>{title}</li>
+            {
+                cities.map(city => {
+                    return (
+                        <DataItem
+                            key={city.name}
+                            name={city.name}
+                            onSelect={onSelect}
+                        />
+                    )
+                })
+            }
         </ul>
     )
 })
@@ -54,21 +60,28 @@ SectionItem.propTypes = {
 }
 
 const DataList = memo(function DataList(props) {
-    const { sections, onSelect,} = props
+    const { sections, onSelect, toAlpha } = props
 
     return (
         <div className="data-list">
             <div className='data-cate'>
                 {
-                    sections.map(section=> {
+                    sections.map(section => {
                         return (
                             <SectionItem
-                                key = {section.title}
-                                title = {section.title}
-                                cities = {section.citys}
-                                onSelect = {onSelect}
+                                key={section.title}
+                                title={section.title}
+                                cities={section.citys}
+                                onSelect={onSelect}
                             />
                         )
+                    })
+                }
+            </div>
+            <div className='data-index'>
+                {
+                    alphabet.map(alpha => {
+                        return <AlphaIndex key={alpha} alpha={alpha} onClick={toAlpha} />
                     })
                 }
             </div>
@@ -79,27 +92,53 @@ const DataList = memo(function DataList(props) {
 DataList.propTypes = {
     sections: PropTypes.array.isRequired,
     onSelect: PropTypes.func.isRequired,
-} 
+    toAlpha: PropTypes.func.isRequired,
+}
+
+const alphabet = Array.from(new Array(26), (ele, index) => {
+    return String.fromCharCode(65 + index)
+})
+
+const AlphaIndex = memo(function AlphaIndex(props) {
+    const { alpha, onClick } = props
+
+    return (
+        <i className='index-item' onClick={() => onClick(alpha)}>
+            {alpha}
+        </i>
+    )
+})
+
+AlphaIndex.propTypes = {
+    alpha: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
+}
+
 const Selector = memo(function Selector(props) {
     const { show, selectedData, isLoading, onBack, fetchData, onSelect } = props
     const [searchKey, setSearchKey] = useState('')
-    const key = useMemo(()=> searchKey.trim(), [searchKey])
+    const key = useMemo(() => searchKey.trim(), [searchKey])
 
-    useEffect(()=> {
+    useEffect(() => {
         console.log(selectedData)
-        if(!show || selectedData || isLoading) return 
+        if (!show || selectedData || isLoading) return
         fetchData()
     }, [show, selectedData, isLoading, fetchData])
 
-    const ouputCitySections = ()=> {
-        if(isLoading) {
+    const toAlpha = useCallback(alpha => {
+        document.querySelector(`[data-cate='${alpha}']`).scrollIntoView()
+    }, [])
+
+    const ouputCitySections = () => {
+        if (isLoading) {
             return <div>Loading</div>
-        } 
-        if(selectedData) {
+        }
+        if (selectedData) {
             return (
                 <DataList
-                    sections = {selectedData.cityList}
-                    onSelect = {onSelect}
+                    sections={selectedData.cityList}
+                    onSelect={onSelect}
+                    toAlpha={toAlpha}
                 />
             )
         }
