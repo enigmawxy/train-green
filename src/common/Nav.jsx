4d4ed24cf43485da4526bf7fc80b2fd69fa8af70
enigmawxy <1480973357@qq.com>
2020-08-14 21:25:07 +0800

    90102

---
+++ src/common/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Nav.css'
+
+export default function Nav(props) {
+    return (
+        <div></div>
+    )
+}
+
+Nav.propTypes = {
+
+}
---
+++ src/query/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './List.css'
+
+export default function List(props) {
+    return (
+        <div></div>
+    )
+}
+
+List.propTypes = {
+
+}
